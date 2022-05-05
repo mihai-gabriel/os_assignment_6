9222f3e9e2d47660d5c8b51396f416e65e8ebace
Mihai Gabriel <17096004+mihai-gabriel@users.noreply.github.com>
2022-05-06 01:11:06 +0300

    saving changes

--- ex3.c
+++ ex3.c
@@ -16,34 +16,6 @@
 
 int main(int argc, char* argv[])
 {
-    int pid = fork();
-    if (pid == -1)                      // fork() failed
-    {
-        perror("fork() error: ");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0)                       // in child process
-    {
-        printf("\n[In CHILD] I started ...\n");
-
-        int i;
-        for (i = 0; i < 3; i++)
-        {
-            sleep(5);
-            printf("[In CHILD]\tMy PID is %d Parent PID is %d\n", getpid(), getppid());
-        }
-
-        printf("[In CHILD] I finished my job.\n");
-    }
-    else                                // in parent process
-    {
-        signal(SIGCHLD, SIG_IGN);       // ignore the SIGCHLD signal
-        printf("[In PARENT] I started ...\n");
-        printf("[In PARENT]\tMy PID is %d Child PID is %d\n", getpid(), pid);
-        while(1);
-        printf("[In PARENT] I finished.\n");
-    }
-
+    printf("Test");
     return 0;
 }
---
+++ send.sh
@@ -0,0 +1,3 @@
+git add -A;
+git commit -m "saving changes";
+git push -u origin main;
